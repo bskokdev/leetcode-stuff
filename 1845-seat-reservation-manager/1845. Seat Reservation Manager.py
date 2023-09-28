a41f6ce43fd184f7f8c14f60e66828f6829ae895
Boris Skok <74473558+bskokdev@users.noreply.github.com>
2023-09-28 06:25:04 -0700

    Runtime: 479 ms (faster than 15.5%), Memory: 44.1 MB (less than 58.63%) - GitSync

---
+++ 1845-seat-reservation-manager/1845. Seat Reservation Manager.py
@@ -0,0 +1,17 @@
+class SeatManager:
+
+    def __init__(self, n: int):
+        self.pq = [i for i in range(1, n+1)]
+        heapq.heapify(self.pq)
+
+    def reserve(self) -> int:
+        return heapq.heappop(self.pq)
+
+    def unreserve(self, seatNumber: int) -> None:
+        heapq.heappush(self.pq, seatNumber)
+
+
+# Your SeatManager object will be instantiated and called as such:
+# obj = SeatManager(n)
+# param_1 = obj.reserve()
+# obj.unreserve(seatNumber)
