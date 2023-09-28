b3d3c43a9de903398a1f40aca3520838774d8618
Boris Skok <74473558+bskokdev@users.noreply.github.com>
2023-09-28 06:09:34 -0700

    Runtime: N/A , Memory: N/A  - GitSync

---
+++ 415-add-strings/415. Add Strings.py
@@ -0,0 +1,8 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        # convert string to num
+        # add them -> return added num as a string
+        a = int(num1)
+        b = int(num2)
+        
+        return str(a+b)
